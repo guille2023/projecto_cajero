665cdd069adcad0efb0831a61e6790d82e3ab26d
guille_c <guillecajal@gmail.com>
2022-11-22 00:14:39 -0300

    proyecto cajero version 1.0

---
+++ funcionesCajero.py
@@ -0,0 +1,172 @@
+import os
+
+def presentacion():
+    print("♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦  ♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦  ♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦\n")
+    print("\t\t\t\tHola Bienvenido al cajero de codo a codo \n")
+    print("ingresa tu clave para acceder a tu cuenta \n la clave es 1234 ")
+    return print("♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦  ♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦  ♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦\n")
+
+
+
+
+def menu(nombre):
+    print("#########################################")
+    print("Bienvenido a tu cuenta", nombre, "!!")
+    print("#########################################")
+    print("seleciona una opcion y presiona lo Enter: ")
+    print("#########################################")
+    print("     #1  consultar saldo")
+    print("     #2  depositar dinero")
+    print("     #3  extraer dinero")
+    print("     #4  transferir dinero")
+    print("     #5  comprar dolares")
+    print("     #6  vender dolares")
+    print("     #7  consultar CBU")
+    print("     #8  simulador plazo fijo")
+    print("     #10  ultimos movimientos")
+    print("     #10  salir de la cuenta")
+    return print("#########################################")
+
+
+# 1- consultar saldo, utilice la funcion lambda
+saldoCA = lambda saldo : print("tu saldo actual en pesos es: $" ,saldo, "\n")
+cuentaDolar = lambda saldoDolar : print("tu saldo actual en dolares es: u$S" ,saldoDolar, "\n")
+
+# 2- ingresar dinero
+def ingresar(saldo, ingreso):
+    saldo += ingreso
+    return print("--Gracias por ingresar su dinero, su saldo actual es de: $",saldo, "--")
+
+# 3- extraer de la cuenta
+def extraer(saldo,extraccion,saldoActual):
+    
+    if (saldo >= extraccion) and (extraccion >= 500):
+        saldoActual = saldo - extraccion
+        return print("gracias por extraer, tu saldo restante es: $", saldoActual )
+    elif(saldo >= extraccion) and (extraccion < 500):
+        return print("el retiro minimo es de 500")
+
+    else:
+        return print("saldo insuficiente ", saldo)
+
+
+# 4- transferir dinero
+def transferi(tranferir,monto,saldo,saldoActual):
+    print("#########################################################")
+
+    print("Estas por realizar una transferencia al numero de cuenta ", tranferir , "con el siguiente monto: ", monto )
+    print("estas seguro que deseas realizar esta accion ?")
+
+    print("#########################################################")
+    if(saldo >=tranferir):
+        saldoActual = saldo - monto
+        confirmar = str(input("ingresa: \n     # si para confirmar la transferencia. \n     # no para cancelar: \n "))
+        if confirmar == "si":
+            print("gracias tu tranferencia ha sido realizada!, tu saldo actual es de: $" , saldoActual )
+        elif confirmar == "no":
+            print("has cancelado tu transferencia")
+        else:
+            print("has ingresado un valor invalido")
+    else:print("saldo insuficiente")            
+
+# 5- comprar dolares
+def comprarDolares(saldo):
+    print("#####################################")
+    print("    el precio del dolar es de $160")
+    print("    tu saldo es : $" , saldo)
+    return print("#####################################")
+
+
+
+def comprarDolares1(compraDolar,saldo,saldoDolar):
+    print("#####################################")
+    print("estas seguro de comprar : u$s" , compraDolar, " dolares ?")
+    confirma  = str(input("ingresa \n     #si para confirmar. \n     #no para cancelar "))
+    print(" ")
+    if confirma == "si":
+        conversion = compraDolar * 160
+        saldoActual1 = saldo - conversion
+        if(saldoActual1 >= 0):
+            saldoDolar = saldoDolar + compraDolar
+            print("#####################################################")
+
+            print("tu saldo en tu cuenta pesos es de: $" , saldoActual1)
+            print("tu saldo en tu cuenta dolares es de: u$s" , saldoDolar )
+
+            return print("#####################################################")
+            
+            
+            
+        else: saldo = saldoActual1 + conversion 
+        print("#####################################################")
+
+        print("tu saldo en tu cuenta dolares es de: u$s" , saldoDolar )
+        print("tu saldo en tu cuenta pesos es de: $" , saldo)
+
+        print("#####################################################")
+
+        return print("saldo insuficiente para comprar dolares ")
+
+
+
+    elif confirma == "no":
+        return print("has cancelado tu compra")       
+
+# vender dolares
+def venderDolares(venderDolar,saldo,saldoDolar):
+    print("#####################################")
+    print("estas seguro de vender : u$s" , venderDolar, " dolares ?")
+    confirma  = str(input("ingresa \n     #si para confirmar. \n     #no para cancelar "))
+    print(" ")
+    if confirma == "si":
+        if(saldoDolar >= venderDolar):
+            conversion = venderDolar * 155
+            saldoActual2 = saldo + conversion
+        
+            saldoDolar = saldoDolar - venderDolar
+            print("#####################################################")
+
+            print("tu saldo en tu cuenta pesos es de: $" , saldoActual2)
+            print("tu saldo en tu cuenta dolares es de: u$s" , saldoDolar )
+
+            return print("#####################################################")        
+
+        else: 
+            print("#####################################################")
+
+            print("tu saldo en tu cuenta dolares es de: u$s" , saldoDolar )
+            print("tu saldo en tu cuenta pesos es de: $" , saldo)
+
+            print("#####################################################")
+
+            return print("saldo insuficiente para vender dolares ")
+
+def consultarCbu():
+    return print("Numero de CBU: " ,2850590 ,1907000, 12345678)
+
+# 8 crear plazo fijo en pesos
+
+
+
+def simularPlazoFijo(plazoFijo,cantidaDia,tna):
+    if (plazoFijo >= 1000) and (cantidaDia >= 30):
+        porcentajePlazo = tna * (cantidaDia /365)
+        gananciaPLazo = plazoFijo * porcentajePlazo + plazoFijo
+
+        print("♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦  ♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦  ♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦\n")
+        print(" Monto a invertir en pesos: $",plazoFijo)
+        print("Durante ",cantidaDia," dias")
+        print("La ganancia Total es de: $",round(gananciaPLazo, 2))  
+        return print("♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦  ♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦  ♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦")
+    
+    elif(plazoFijo < 1000) and (cantidaDia >= 30):
+        return print("Error, Ingrese un monto mayor a 1000")
+
+    else: print("Error, La cantidad de dias es inferior a 30 dias")
+
+
+# limpiar pantalla
+cls = lambda: os.system ("cls")
+
+# funcion esperar
+esperar = lambda:type(input("presione enter para continuar......"))
---
+++ proyecto_cajero.py
@@ -0,0 +1,130 @@
+
+from  funcionesCajero import *
+import os
+
+
+def cajero():
+    
+    saldo = float(82500)
+    saldoDolar = float(100)
+    saldoActual = float(0)
+    tna = float(0.75)
+    
+        
+    presentacion()
+        
+    clave = int(input("ingresa tu clave: "))
+    nombre = str(input("ingresa tu nombre: "))
+        
+    cls()
+    if clave == 1234:
+
+        
+        opcion = 0
+        while opcion < 10:
+            menu(nombre)
+
+            opcion = int(input("ingresa tu opcion: "))
+            cls()
+
+
+            #1consultar saldo
+            if opcion == 1:
+                
+                saldoCA(saldo)
+                cuentaDolar(saldoDolar)
+                esperar()
+                cls()
+
+            #2depositar dinero
+            elif opcion == 2:
+                
+                ingreso = int(input("digite por teclado el monto de su dinero a ingresar y luego inserte su dinero: "))
+                
+                ingresar(saldo,ingreso)
+                esperar()
+                cls()
+            #3extraer dinero
+            elif opcion == 3:
+
+                extraccion = int(input("ingresa el monto a extraer: "))
+
+                extraer(saldo,extraccion,saldoActual)
+
+            #4transferir dinero
+            elif opcion == 4:
+                tranferir = int(input("ingrese el cbu de la cuenta a la cual deseas tranferir: "))
+                monto = int(input("ingresa el monto a tranferir: "))
+                
+                transferi(tranferir,monto,saldo,saldoActual)
+
+
+            #5comprar dolares
+            elif opcion == 5:
+                comprarDolares(saldo)
+
+                compraDolar = float(input("ingresa el monto de dolares a comprar: "))
+
+
+                comprarDolares1(compraDolar,saldo,saldoDolar)      
+            #6vender dolares
+            elif opcion == 6:
+
+                venderDolar = float(input("ingresa el monto de dolares a vender: "))
+                venderDolares(venderDolar,saldo,saldoDolar)
+
+
+            #7 consultar CBU
+            elif opcion == 7:
+                consultarCbu()
+
+            # 8 crear plazo fijo en pesos
+            elif opcion == 8:
+                
+                print("♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦  ♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦  ♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦\n")
+                print ("El monto minimo a invertir es de 1000 pesos \n")
+
+                plazoFijo = float(input("Ingresa el monto a invertir en plazo fijo: \n"))
+                print("♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦  ♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦  ♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦\n")
+
+                
+                print("El tiempo minimo es de 30 dias\n")
+
+                cantidaDia = int(input("Durante cuanto dias: \n"))
+                print("♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦  ♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦  ♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦\n")
+                
+
+                simularPlazoFijo(plazoFijo, cantidaDia, tna)
+
+            
+
+            #9ver ultimos movimientos
+
+            #10salir de la cuenta
+            elif opcion == 10:
+                pass
+    else:
+        print("clave incorrecta")
+    print("========================================================\n")
+    print("Gracias por utilizar los servicios del cajero automatico \n")
+    print("========================================================")
+cajero()
+
+
+
+
+
+# 1- utilice dos modulos una para las funciones y otro para el programa principal
+# 2- reorganize y cree funciones para que el programa principal sea mas limpio en codigo
+# 3- reescribi  la funcion extraer para que no quede un saldo negativo y tenga un monto minimo de retiro
+# 4- reescribi la funcion tranferi para que no transfiera mas dinero del que tiene
+# 5- reescribi la funcion comprar dolares1 para que no compre de lo que le permite el saldo
+# 6- cree la funcion vender dolares, con la condicion de que no venda mas dolares del que tiene en la cuenta
+# 7- creo la funcion consultar CBU
+# 8- en este caso solo cree un funcion que simula un plazo fijo. defini a tna como variable global .
+# 9- creo una funcion lambda para limpiar la pantalla. solo funciona para widonws en caso de otros S.O hay que reescribirla
+# 10- elimine la opcion de elegir idioma , ya que me parecio que es reescribir codigo
+# 11- agrego un iterador while , para interartuar con el programa
+# 12- agrego la funcion salir del programa
+# 13- creo una funcion lambda esperar para que la interaccion con el programa no sea tan brusca 
+# 14- utilizo git 
